f9bbe9a4ae3144069ff983a0291a5820ca50dada
JPMLMQ <juanmello@id.uff.br>
2024-11-20 18:51:55 -0300

    sismograma
    Conferir

--- acousticwave.py
+++ acousticwave.py
@@ -2,51 +2,63 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import ricker
 
-L = 1.0          
-T = 0.1                  
-dx = 0.05          
+L = 100          
+T = 1                  
+dx = 1          
 dt = 0.01      
 
 nx = int(L / dx)  
 nt = int(T / dt) 
 f0 = 30
 n = 2
-sigma = n / (2 * np.pi * f0)
 
-def ricker(t, sigma):
-    return (1 -(t/sigma)**2)*np.exp(-(t)**2/(2*sigma**2))
+def ricker(t, f0):
+    return (1 - 2 * (np.pi * f0* t)**2)*np.exp(-(np.pi * f0* t)**2)
 
 x = np.linspace(0, L, nx)
+t = np.linspace(0,T, nt)
 
 def c(x):
-    return 2 + x/3
+    return 2000
 
 u = np.zeros(nx)
 u_anterior = np.zeros(nx)
 u_posterior = np.zeros(nx)
 
-posição = int(nx / 2)
-for i in range(nx):
-    u[i] = ricker(x[i] - x[posição], sigma)
+F = np.zeros(nt)
+tlag = 0.04
 
-soluções = np.zeros((nx, nt))
+for i in range(nt):
+    F[i] = ricker(t[i]- tlag, f0)
+
+solution = np.zeros((nx, nt))
+isx = int(nx/2)
 
 for t in range(nt):
     for i in range(nx-1):
         R = (c(x[i])*dt/dx)**2
-        u_posterior[i] = R*u[i-1] - 2*(R-1)*u[i] + R*u[i+1] - u_anterior[i]
-
+        u_posterior[isx] = F[t]
+        u_posterior[i] += R*u[i-1] - 2*(R-1)*u[i] + R*u[i+1] - u_anterior[i]
+        
     u_anterior = u.copy()
     u = u_posterior.copy()
     
-    soluções[:, t] = u
+    solution[:, t] = u
 
-snapshot = int(nt/2)
+snapshot = 0
 
-
-plt.plot(x, soluções[:,snapshot], label=f"tempo(t={snapshot}s)")
+plt.plot(x, solution[:,snapshot], label=f"tempo(t={snapshot}s)")
 plt.xlabel("x")
 plt.ylabel("u")
 plt.legend()
 plt.grid(True)
 plt.show()
+
+k = np.max(np.abs(solution))
+
+plt.imshow(solution, cmap="seismic", aspect="auto", extent=[0, L, T, 0], vmin=-k, vmax=k)
+plt.colorbar(label='Amplitude')
+plt.title("Sismograma")
+plt.tight_layout()
+plt.show()
+
