1a8944db1e4b8057e71ddca54d2323bc6201792b
JPMLMQ <juanmello@id.uff.br>
2024-12-20 13:05:20 -0300

    função de atenuação, alguns erros

--- acousticwave2D.py
+++ acousticwave2D.py
@@ -12,12 +12,11 @@ def parametros(L, H, T, dx, dz, dt):
     t = np.linspace(0, T, nt)
     return x, z, t, nx, nz, nt
     
-def v(nx, nz):
+def v(nx, nz, v1=1500, v2=2000, v3=3000):
     vp = np.zeros([nz,nx])
-    vp[:,:]= 1500
-    # vp[0:int(nz/4),:]= 1500
-    # vp[int(nz/4):int(nz/2),:] = 2000
-    # vp[int(nz/2):nz,:] = 3000
+    vp[0:int(nz/4),:]= v1
+    vp[int(nz/4):int(nz/2),:] = v2
+    vp[int(nz/2):nz,:] = v3
     return vp
 
 def ricker(f0, t):
@@ -33,6 +32,22 @@ def ondas(nx,nz):
     u_posterior = np.zeros((nz,nx))
     return u_anterior, u, u_posterior
 
+def borda (nx,nz,fator = 0.015, N = 50):
+    A = np.ones((nz, nx))  
+    for i in range(nx):
+        for j in range(nz):
+            if i < N: 
+                A[j, i] *= np.exp(-((fator * (N - i)) ** 2))
+            elif i >= nx - N:  
+                A[j, i] *= np.exp(-((fator * (i - (nx - N))) ** 2))
+
+            if j < N:  
+                A[j, i] *= np.exp(-((fator * (N - j)) ** 2))
+            elif j >= nz - N:  
+                A[j, i] *= np.exp(-((fator * (j - (nz - N))) ** 2))
+
+    return A
+
 @numba.jit(parallel=True, nopython=True)
 def marcha_no_espaço(u_anterior, u, u_posterior, nx, nz, c, dt, dx, dz):
     if np.any(((c*dt/dx)**2)> 1) or np.any(((c*dt/dz)**2)> 1):
@@ -44,14 +59,15 @@ def marcha_no_espaço(u_anterior, u, u_posterior, nx, nz, c, dt, dx, dz):
             u_posterior[j, i] = (c[j, i] ** 2) * (dt ** 2) * (pxx + pzz) + 2 * u[j, i] - u_anterior[j, i]
     return u_posterior
 
-def marcha_no_tempo(u_anterior, u, u_posterior, source, nt, nx, nz, c, recx, recz,dt):
-    isx = int(nx/2)
+def marcha_no_tempo(u_anterior, u, u_posterior, source, nt, nx, nz, c, recx, recz,dt, A):
+    isx = int(nx-20)
     isz = 0
     sism = np.zeros((nt, nx)) 
     fig, ax = plt.subplots(figsize=(10, 10))  
     for k in range(nt):
         u[isz,isx]= u[isz,isx] + source[k]*(dt*c[isz, isx])**2
-        u_posterior = marcha_no_espaço(u_anterior, u, u_posterior, nx, nz, c, dt, dx, dz)
+        u_posterior = marcha_no_espaço(u_anterior, u, u_posterior, nx, nz, c, dt, dx, dz) 
+        u_posterior *= A
         u_anterior = np.copy(u)
         u = np.copy(u_posterior)
 
@@ -86,5 +102,6 @@ source = ricker(f0, t)
 u_anterior, u, u_posterior = ondas(nx,nz)
 recx= range(nx)
 recz = 10
-sism = marcha_no_tempo(u_anterior, u, u_posterior, source, nt, nx, nz, c, recx, recz, dt)
+A = borda(nx, nz, fator=0.015, N=50)
+sism = marcha_no_tempo(u_anterior, u, u_posterior, source, nt, nx, nz, c, recx, recz, dt, A)
 plot_sismograma(sism)
