df0167d227da49b1f861f8129b267e338e8ed8f1
JPMLMQ <juanmello@id.uff.br>
2024-12-24 01:09:21 -0300

    Update acousticwave2D.py

--- acousticwave2D.py
+++ acousticwave2D.py
@@ -3,14 +3,16 @@ import matplotlib.pyplot as plt
 import numba
 from numba import jit
 
-def parametros(L, H, T, dx, dz, dt):
+def parametros(L, H, T, dx, dz, dt, N):
     nx = int(L/dx) + 1
     nz = int(H/dz) + 1
+    nx_abc = nx + 2*N
+    nz_abc = nz + 2*N
     nt = int(T/dt) + 1
     x = np.linspace(0, L, nx)
     z = np.linspace(0, H, nz)
     t = np.linspace(0, T, nt)
-    return x, z, t, nx, nz, nt
+    return x, z, t, nx, nz, nx_abc, nz_abc, nt
     
 def v(nx, nz, v1=1500, v2=2000, v3=3000):
     vp = np.zeros([nz,nx])
@@ -18,6 +20,21 @@ def v(nx, nz, v1=1500, v2=2000, v3=3000):
     vp[int(nz/4):int(nz/2),:] = v2
     vp[int(nz/2):nz,:] = v3
     return vp
+
+def expand_vp(v,nx_abc,nz_abc, N):
+    v_expand = np.zeros((nz_abc, nx_abc))
+    v_expand[N:nz_abc-N, N:nx_abc - N] = v
+    v_expand[0:N,N: nx_abc - N]= v[0, :]
+    v_expand[nz_abc - N : nz_abc, N: nx_abc - N]= v[-1, :]
+    v_expand[N:nz_abc-N, 0:N] = v[:, 0][:, np.newaxis]
+    v_expand[N:nz_abc-N, nx_abc-N:nx_abc] = v[:, -1][:, np.newaxis]
+    v_expand[0:N, 0:N] = v[0, 0]  
+    v_expand[0:N, nx_abc-N:nx_abc] = v[0, -1] 
+    v_expand[nz_abc-N:nz_abc, 0:N] = v[-1, 0]  
+    v_expand[nz_abc-N:nz_abc, nx_abc-N:nx_abc] = v[-1, -1]
+
+    return v_expand
+
 
 def ricker(f0, t):
     pi = np.pi
@@ -36,7 +53,7 @@ def borda (nx,nz,fator = 0.015, N = 50):
     A = np.ones((nz, nx))  
     for i in range(nx):
         for j in range(nz):
-            if i < N: 
+            if i <= N: 
                 A[j, i] *= np.exp(-((fator * (N - i)) ** 2))
             elif i >= nx - N:  
                 A[j, i] *= np.exp(-((fator * (i - (nx - N))) ** 2))
@@ -60,15 +77,16 @@ def marcha_no_espaço(u_anterior, u, u_posterior, nx, nz, c, dt, dx, dz):
     return u_posterior
 
 def marcha_no_tempo(u_anterior, u, u_posterior, source, nt, nx, nz, c, recx, recz,dt, A):
-    isx = int(nx-20)
-    isz = 0
+    isx = int(nx/2)
+    isz = 100
     sism = np.zeros((nt, nx)) 
     fig, ax = plt.subplots(figsize=(10, 10))  
     for k in range(nt):
         u[isz,isx]= u[isz,isx] + source[k]*(dt*c[isz, isx])**2
         u_posterior = marcha_no_espaço(u_anterior, u, u_posterior, nx, nz, c, dt, dx, dz) 
         u_posterior *= A
-        u_anterior = np.copy(u)
+        u_anterior = np.copy(u) 
+        u_anterior *= A
         u = np.copy(u_posterior)
 
         sism[k, recx] = u[recz, recx]
@@ -95,13 +113,18 @@ H = 3000
 dz = 5               
 dx = 5         
 dt = 0.001 
-x, z, t, nx, nz, nt = parametros(L, H, T, dx, dz, dt)
+N = 50
+x, z, t, nx, nz, nx_abc, nz_abc, nt = parametros(L, H, T, dx, dz, dt, N)
 c = v(nx,nz)
+c_expand = expand_vp(c,nx_abc,nz_abc, N)
+# plt.figure()
+# plt.imshow(c_expand,aspect='equal')
+# plt.show()
 f0 = 60
 source = ricker(f0, t)
 u_anterior, u, u_posterior = ondas(nx,nz)
 recx= range(nx)
-recz = 10
-A = borda(nx, nz, fator=0.015, N=50)
-sism = marcha_no_tempo(u_anterior, u, u_posterior, source, nt, nx, nz, c, recx, recz, dt, A)
+recz = N + 10
+A = borda(nx, nz, fator=0.015, N = 50)
+sism = marcha_no_tempo(u_anterior, u, u_posterior, source, nt, nx, nz, c_expand, recx, recz, dt, A)
 plot_sismograma(sism)
